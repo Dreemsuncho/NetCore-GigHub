854d845afffbddef235e124ad2653c5b8f7fdaed
Elvis Arabadjiyski <dreemsuncho@students.softuni.bg>
2018-04-08 18:17:18 +0300

    Integrate Angular skelet and bootstrap-4

---
+++ NetCore-GigHub/Controllers/GigsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NetCore_GigHub.Controllers
+{
+    //[Authorize]
+    public class GigsController : Controller
+    {
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ NetCore-GigHub/gulpfile.js
@@ -0,0 +1,25 @@
+﻿
+
+var gulp = require("gulp");
+
+var paths = {
+    srcBootstrapCSS: './node_modules/bootstrap/dist/**/*.min.css',
+    srcBootstrapJS: './node_modules/bootstrap/dist/**/*.min.js',
+
+    srcJQueryJS: './node_modules/jquery/dist/**/*.min.js',
+};
+
+gulp.task("default", ["bootstrap", "jquery"]);
+
+gulp.task('bootstrap', function () {
+    gulp.src([
+        paths.srcBootstrapCSS,
+        paths.srcBootstrapJS
+    ]).pipe(gulp.dest("./wwwroot/lib/bootstrap"));
+});
+
+gulp.task('jquery', function () {
+    gulp.src([
+        paths.srcJQueryJS
+    ]).pipe(gulp.dest("./wwwroot/lib/jquery"));
+});
